70af01fcf95b2e4661102dcfc65d48b7120ec441
Stacypep <perpetua.nwosu91@gmail.com>
2023-04-20 17:50:20 -0700

    C Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
 * main - Prints sizes of characters 
-*
 * Return: Always 0 (Success) 
 */
 int main(void)
-{	
+{
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
