6bc14c87a075d69457257a840eaa00b891394016
Stacypep <perpetua.nwosu91@gmail.com>
2023-04-24 14:48:21 -0700

    C variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,18 @@ int main(void)
 {
 	int d, p, q;
 
-	for (d = '0'; d < '7'; d++)
+	for (d = '0'; d < '8'; d++)
 	{
-		for (p = d + 1; p <= '7'; p++)
+		for (p = d + 1; p <= '8'; p++)
 		{
-			for (q = d + 1; q <= '7'; q++)
+			for (q = d + 1; q <= '8'; q++)
 			{
 				if ((p != d) != q)
 				{
 					putchar(d);
 					putchar(p);
 					putchar(q);
-					if (d == '6' && p == '7')
+					if (d == '7' && p == '8')
 					continue;
 				}
 			}
