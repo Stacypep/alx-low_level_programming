90ba6434ee9dda93ba4195a6608b5a95df208eaf
Stacypep <perpetua.nwosu91@gmail.com>
2023-04-25 16:39:37 -0700

    C Function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,16 +14,17 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-		       	n = i * j;
+			n = i * j;
+
 			if ((n / 10) == 0)
 			{
-			       if (j != 0)
+				if (j != 0)
 					_putchar(' ');
 					_putchar(n + '0');
 				if (j == 9)
 					continue;
-				_putchar(',');
-				_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 			}
 			else
 			{
@@ -31,10 +32,10 @@ void times_table(void)
 				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
-				_putchar(',');
-				_putchar(' ')
+					_putchar(',');
+					_putchar(' ');
 			}
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
