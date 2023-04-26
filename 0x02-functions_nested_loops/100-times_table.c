ffacc03382e41363a054979db33ebef68058855d
Stacypep <perpetua.nwosu91@gmail.com>
2023-04-26 03:00:34 -0700

    C nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,10 +4,8 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-
 void print_times_table(int n)
-{
-	int i, j, k;
+{int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -19,10 +17,9 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} 
-				else if (k < 10 && j != 0)
+				} else if (k < 10 && j != 0)
 				{
-					_putchar(',')
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
@@ -35,15 +32,14 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-			       	else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				} else if (k >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 			}
 			_putchar('\n');
 		}
