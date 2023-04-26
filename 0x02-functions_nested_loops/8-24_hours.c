d1f6c2e76e3aa98d43953cb1acab94964eb4047c
Stacypep <perpetua.nwosu91@gmail.com>
2023-04-26 03:55:15 -0700

    C nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,15 +5,11 @@
  *
  * starting from 00:00 to 23:59
  */
-
 void jack_bauer(void)
-{
-	int i, j;
-	
+{int i, j;
 	i = 0;
 	while (i < 24)
-	{
-		j = 0;	
+{j = 0;
 		while (j < 60)
 	{
 		_putchar((i / 10) + '0');
