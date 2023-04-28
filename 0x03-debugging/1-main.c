a81bb5030d8ffa1427938949be191dcef5da9b47
Stacypep <perpetua.nwosu91@gmail.com>
2023-04-28 14:32:18 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - causes an infinite loop
+*
 * Return: 0
 */
 
@@ -11,10 +12,13 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 i = 0;
+
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
 /*}*/
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
