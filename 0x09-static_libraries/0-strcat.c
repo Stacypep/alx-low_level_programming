f8cce50b6c63d5d637553ce10ef57c5ff27393f0
Stacypep <perpetua.nwosu91@gmail.com>
2023-05-12 18:48:23 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+ /**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: pointer to the destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	/* implementation of function */
+
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
